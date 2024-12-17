6ed89e7fe5811cecb711cedcaf873fc822d1dc81
--global <--global>
2024-12-17 14:52:44 +0530

    created first dag

---
+++ dags/my_first_dag.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+
+default_args={
+    'owner': 'srush',
+    'retries':5,
+    'retry_delay': timedelta(minutes=2)
+}
+
+with DAG(
+    dag_id='my_first_dag',
+    default_args=default_args,
+    description='This is my first dag',
+    start_date=datetime(2024,12,17,15),
+    schedule_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command='echo hello world'
+    )
