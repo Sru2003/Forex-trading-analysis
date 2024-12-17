aa7205d79e733bb3961215d8be7bb783f4ca3632
--global <--global>
2024-12-17 15:50:48 +0530

    task2 and task3 run together after task1

--- dags/my_first_dag.py
+++ dags/my_first_dag.py
@@ -9,13 +9,25 @@ default_args={
 }
 
 with DAG(
-    dag_id='my_first_dag',
+    dag_id='my_first_dag_v3',
     default_args=default_args,
     description='This is my first dag',
-    start_date=datetime(2024,12,16,15),
-    schedule_interval='@daily'
+    start_date=datetime(2024,12,16),
+    schedule_interval=timedelta(minutes=1)
 ) as dag:
     task1 = BashOperator(
         task_id='first_task',
         bash_command='echo hello world'
     )
+
+    task2 = BashOperator(
+            task_id='second_task',
+            bash_command='echo task2 runs after task1'
+        )
+
+    task3 = BashOperator(
+        task_id='third_task',
+        bash_command='echo task3 runs after task1 and with task2'
+    )
+    task1.set_downstream(task2)  
+    task1.set_downstream(task3) 
